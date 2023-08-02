f57af21256576d75f0f3627571ba6ce48782f569
Perdente <loserjoker1997@gmail.com>
2023-08-02 23:16:56 +0600

    End of Section 3

---
+++ API/Program.cs
@@ -0,0 +1,55 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+// https://www.youtube.com/watch?v=S7SdtcIr28s&ab_channel=Tech%26CodewithPhi
+builder.Services.AddDbContext<StoreContext>(opt => opt.UseSqlite(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+)); 
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting();
+
+app.UseCors(options => options.WithOrigins("http://localhost:3000") .AllowAnyHeader()
+.AllowAnyMethod());
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+try {
+    context.Database.Migrate();
+    DbInitializer.Initilize(context);
+}
+catch (Exception ex) {
+    logger.LogError(ex, "Problem migrating data");
+}
+
+
+app.Run();
